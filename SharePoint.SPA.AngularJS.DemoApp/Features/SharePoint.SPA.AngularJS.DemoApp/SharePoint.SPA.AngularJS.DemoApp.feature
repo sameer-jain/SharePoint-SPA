8fd63ffd9b935cd2a5830bd5d76b5930f5703b77
unknown <kushal1458@kushal1458.Nagarro.local>
2015-03-03 11:14:06 +0530

    first commit

---
+++ SharePoint.SPA.AngularJS.DemoApp/Features/SharePoint.SPA.AngularJS.DemoApp/SharePoint.SPA.AngularJS.DemoApp.feature
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="9786ca91-5f24-4fd0-9269-cd5813ecd899" description="SharePoint App Feature" featureId="9786ca91-5f24-4fd0-9269-cd5813ecd899" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="SharePoint.SPA.AngularJS.DemoApp Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="f2f06a80-5fc2-45b5-85ee-cc8803804a41" />
+    <projectItemReference itemId="40318823-badd-49a6-9d4b-120761ba5ab6" />
+    <projectItemReference itemId="dd6a5749-a6e4-4bd9-88e8-417b577c3fe8" />
+    <projectItemReference itemId="9a2dc7ac-d9a3-4169-b1cc-ad8f6ed30832" />
+    <projectItemReference itemId="9e97fb36-e553-4b7f-9465-597a00aa4db3" />
+    <projectItemReference itemId="026ab2e5-6e3f-430f-ab0f-7cfc5bb7576e" />
+    <projectItemReference itemId="533b9e67-a3a8-4fed-8915-7dc6ffcdc5c7" />
+  </projectItems>
+</feature>
---
+++ SharePoint.SPA.AngularJS.DemoApp/Scripts/App.js
@@ -0,0 +1,250 @@
+﻿
+    'use strict';
+
+    // create app
+    var app = angular.module('employeeApp', ['ngRoute']);
+    // 'ngRoute' for app route (url path) support
+
+    // startup code
+    app.run(['$route',  function ($route) {
+
+    }]);
+
+    //Routing
+    app.config(['$routeProvider',
+      function ($routeProvider) {
+          $routeProvider
+              .when('/', {
+                  templateUrl: '../Views/List.html'
+              })
+              .when('/List', {
+                  templateUrl: '../Views/List.html'
+              })
+              .when('/Add', {
+                  templateUrl: '../Views/Add.html'
+              })
+              .when('/Edit/:Id', {
+                  templateUrl: '../Views/Edit.html'
+              }).otherwise({ redirectTo: '/' });
+      }
+    ]);
+
+
+    //Add form controllers
+    app.controller('AddFormCtrl', 
+    function ($scope, $location, sharePointListServices) {
+        $scope.addEmpClick = function (event) {
+            if ($('form')[0].checkValidity() === false) {
+                event.stopPropagation();
+                return;
+            }
+
+            $scope.isDisabled = true;
+            var emp = new employeeItem();
+            emp.Title = $scope.EmployeeItem.Title;
+            emp.LastName = $scope.EmployeeItem.LastName;
+            emp.MobileNumber = $scope.EmployeeItem.MobileNumber;
+            emp.JobTitle = $scope.EmployeeItem.JobTitle;
+            emp.Email = $scope.EmployeeItem.Email;
+
+            sharePointListServices.addListItem("EmployeeList", emp).then(function (data) {
+                alert("Employee Info submitted successfully.");
+                $location.path('/List');
+            }).then(function () {
+                $scope.EmployeeItem = null;
+            });
+            event.preventDefault();
+        };
+
+        $scope.cancelClick = function (event) {
+            $location.path('/List');
+            event.preventDefault();
+        };
+       
+
+    });
+
+
+    //edit form controllers
+    app.controller('EditFormCtrl', 
+    function ($scope, $location, $route, $routeParams, sharePointListServices) {
+        var itemId = $routeParams.Id || 0;
+        var query = "?$select=ID,Title,LastName,MobileNumber,JobTitle,Email";
+        sharePointListServices.getListItemById("EmployeeList", itemId, query).then(function (data) {
+            $scope.EmployeeItem = data;
+        });
+
+        $scope.updateClick = function (event) {
+            $scope.isDisabled = true;
+            var employeeItem = $scope.EmployeeItem;
+            sharePointListServices.updateListItems("EmployeeList", employeeItem).then(function (data) {
+                alert("Employee data Updated successfully.");
+                $location.path('/List');
+            });
+            event.preventDefault();
+        };       
+
+        $scope.cancelClick = function (event) {
+            $location.path('/List');
+            event.preventDefault();
+        };
+
+
+    });
+
+    //List form controllers
+    app.controller('ListFormCtrl', 
+    function ($scope, $location , sharePointListServices) {
+        $scope.isItemExists = true;
+        var query = "?$select=ID,Title,LastName,MobileNumber,JobTitle,Email";
+        sharePointListServices.getListItems("EmployeeList", query).then(function (result) {
+            $scope.EmployeeItems = result;
+           
+            if (angular.equals([], $scope.EmployeeItems)) {
+                $scope.isItemExists = false;
+            }
+            else {
+                $scope.isItemExists = true;
+            }
+
+        });
+
+        $scope.deleteItem = function (item, event) {
+
+            var wantToDelete = confirm('Are you sure you want to delete?');
+            if (wantToDelete == true) {
+                sharePointListServices.deleteListItem('EmployeeList', item.ID).then(function (data) {
+                    alert("Item Deleted successfully.");                   
+                }).then(function () {
+                    var idx = $scope.EmployeeItems.indexOf(item);
+                    if (idx > -1) {
+                        $scope.EmployeeItems.splice(idx, 1);
+                    }
+
+                    if (angular.equals([], $scope.EmployeeItems)) {
+                        $scope.isItemExists = false;
+                    }
+                    else {
+                        $scope.isItemExists = true;
+                    }
+                });
+            }
+            event.preventDefault();
+        };
+    });
+
+
+
+    app.service('sharePointListServices', ['$http', '$q', function ($http, $q) {
+
+        //Get the ListItem
+        this.getListItemById = function (listTitle, itemId, query) {
+            var dfd = $q.defer();
+            setHttpHeaders($http, actionType.Get);
+            var restUrl = _spPageContextInfo.webServerRelativeUrl+ "/_api/web/lists/getbytitle('" + listTitle + "')/items(" + itemId + ")" + query;
+            $http.get(restUrl).success(function (data) {
+                dfd.resolve(data.d);
+            }).error(function (data) {
+                dfd.reject("error in getting items");
+            });
+            return dfd.promise;
+        }
+
+        //Get the ListItem
+        this.getListItems = function (listTitle, query) {
+            var dfd = $q.defer();
+            setHttpHeaders($http, actionType.Get);
+            var restUrl = _spPageContextInfo.webServerRelativeUrl+ "/_api/web/lists/getbytitle('" + listTitle + "')/items" + query;
+            $http.get(restUrl).success(function (data) {
+                dfd.resolve(data.d.results);
+            }).error(function (data) {
+                dfd.reject("error in getting items");
+            });
+            return dfd.promise;
+        }
+
+        //Create a ListItem
+        this.addListItem = function (listTitle, listItem) {
+            var dfd = $q.defer();
+            setHttpHeaders($http, actionType.Add);
+            var restUrl =_spPageContextInfo.webServerRelativeUrl+ "/_api/web/lists/getbytitle('" + listTitle + "')/items";
+            $http.post(restUrl, listItem).success(function (data) {
+                //resolve the new data
+                dfd.resolve(data.d);
+            }).error(function (data) {
+                dfd.reject("failed to get items");
+            });
+            return dfd.promise;
+        }
+
+        //Update a ListItem
+        this.updateListItems = function (listTitle, listItem) {
+            var dfd = $q.defer();
+            setHttpHeaders($http, actionType.Update);
+            var restUrl =_spPageContextInfo.webServerRelativeUrl+  "/_api/web/lists/getbytitle('" + listTitle + "')/items(" + listItem.ID + ")";
+            $http.post(restUrl, listItem).success(function (data) {
+                //resolve something
+                dfd.resolve(true);
+            }).error(function (data) {
+                dfd.reject("error updating item");
+            });
+            return dfd.promise;
+        }
+
+        //Delete a list item
+        this.deleteListItem = function (listTitle, itemId) {
+            var dfd = $q.defer();
+            setHttpHeaders($http, actionType.Delete);
+            var restUrl = _spPageContextInfo.webServerRelativeUrl+ "/_api/web/lists/getbytitle('" + listTitle + "')/items(" + itemId + ")";
+            $http.post(restUrl)
+                .success(function (data) {
+                    //resolve something
+                    dfd.resolve(true);
+                }).error(function (data) {
+                    dfd.reject("error deleting item");
+                });
+            return dfd.promise;
+        }
+
+    }]);
+
+    var actionType = {
+        Get: 0,
+        Add: 1,
+        Update: 2,
+        Delete: 4
+    };
+
+    function setHttpHeaders($http, action) {
+        //Define the http headers actionType
+        $http.defaults.headers.common.Accept = "application/json;odata=verbose";
+        $http.defaults.headers.post['Content-Type'] = 'application/json;odata=verbose';
+        $http.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+        $http.defaults.headers.post['X-RequestDigest'] = angular.element(document.querySelector('#__REQUESTDIGEST')).val();
+        $http.defaults.headers.post['If-Match'] = "*";
+        var httpMethod;
+        switch (action) {
+            case actionType.Update:
+                httpMethod = "MERGE";
+                break;
+            case actionType.Delete:
+                httpMethod = "DELETE";
+                break;
+            default:
+                httpMethod = "";
+                break;
+        }
+        $http.defaults.headers.post['X-HTTP-Method'] = httpMethod;
+    }
+
+    var employeeItem = function () {
+        this.ID = undefined;
+        this.Title = undefined;
+        this.LastName = undefined;
+        this.MobileNumber = undefined;
+        this.JobTitle = undefined;
+        this.Email = undefined;
+        this.__metadata = {
+            type: 'SP.Data.EmployeeListListItem'
+        };
+    };
